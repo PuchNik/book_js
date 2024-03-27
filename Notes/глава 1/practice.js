6269f925271f07057fd835fc9dfd3aa5d22b0195
PuchNik <i.p.chn.k@gmail.com>
2024-03-27 17:38:50 +0300

    Begin

---
+++ Notes/глава 1/practice.js
@@ -0,0 +1,54 @@
+// Примеры математических операторов
+let a = 10;
+let b = 5;
+
+console.log(a + b); 
+console.log(a - b); 
+console.log(a * b); 
+console.log(a / b); 
+console.log(a % b); 
+console.log(a ** b); 
+
+
+let isTrue = true;
+let isFalse = false;
+
+console.log(isTrue && isFalse); 
+console.log(isTrue || isFalse); 
+console.log(isTrue || isFalse || isTrue);
+
+//Пример специальных чисел
+console.log(NaN);
+console.log(Infinity);
+console.log(-Infinity);
+//---------------------------------------------------------------------------------------------
+
+
+// Примеры строковых форматов
+let name = 'Вася';
+let age = 25;
+
+console.log('Меня зовут ' + name + ', мне'+ age +'лет');
+console.log("Меня зовут ' + name + ', мне'+ age +'лет");
+console.log(`Меня зовут ${name}, мне ${age} лет`);
+
+// Пример экранирования символов в строке
+console.log('Первая строка\nВторая строка');
+console.log('Первая строка\rВторая строка');
+console.log('Первая строка\tВторая строка');
+console.log('Первая строка\r\nВторая строка');
+
+// Пример конкатенации строк
+let str1 = 'Первая строка';
+let str2 = 'Вторая строка';
+
+console.log(str1 + str2);
+console.log(str1.concat(str2));
+//---------------------------------------------------------------------------------------------
+
+
+//Унарный-бинарный-троичный
+
+
+
+
