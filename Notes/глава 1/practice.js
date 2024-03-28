aef743654867365d5dbd81b96029d2bfbe7a9e8b
PuchNik <i.p.chn.k@gmail.com>
2024-03-28 18:11:28 +0300

    End of the first part

--- Notes/глава 1/practice.js
+++ Notes/глава 1/practice.js
@@ -47,8 +47,59 @@ console.log(str1.concat(str2));
 //---------------------------------------------------------------------------------------------
 
 
-//Унарный-бинарный-троичный
+// Унарный оператор typeof (унарный)
+let a = 10;
+let b = '10';
+let c = true;
+let d = null;
+let e = undefined;
+let f = Symbol();
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
 
 
+// оперторы сравнения
+console.log(a > b);
+console.log(a < b);
+console.log(a == b);
+console.log(a != b);
+console.log(a <= b);
+console.log(a >= b);
+console.log(a === b);
+console.log(a !== b);
 
 
+// Логические операторы
+console.log(!a);
+console.log(!!a);
+console.log(!a && !a);
+console.log(!a || !a);
+console.log(!a || !a || !a);
+console.log(!a && !a || !a);
+console.log(!a || !a && !a);
+console.log(!a && !a && !a);
+console.log(!a || !a || !a || !a);
+console.log(!a || !a || !a || !a || !a);
+console.log(!a || !a || !a || !a || !a || !a);
+console.log(!a || !a &&!a ||!a);
+
+
+// Тернарный оператор (троичный)
+let result = (a > b) ? 'a больше b' : 'a меньше b';
+console.log(result);
+//---------------------------------------------------------------------------------------------
+
+
+// Пустые значения null и undefined
+let a = null;
+let b = undefined;
+// ! null == undefined
+// ! null === 0
+
+console.log(a);
+console.log(b);
